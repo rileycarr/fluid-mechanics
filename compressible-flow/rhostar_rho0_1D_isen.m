86065305347b4302cb376e0eb0d6d868c605bc35
Riley Carr <rileydcarr@gmail.com>
2017-03-07 10:31:47 -0500

    Added density star over density0 relation for 1D compressible flow

---
+++ compressible-flow/rhostar_rho0_1D_isen.m
@@ -0,0 +1,8 @@
+function [ rhostar_rho0 ] = rhostar_rho0_1D_isen( k )
+%UNTITLED8 Summary of this function goes here
+%   Detailed explanation goes here
+
+rhostar_rho0 = (2/(k+1))^(1/(k-1));
+
+end
+
